37d048975ee05bb9739bf40e15db584c0155a0c4
Nicolas Ogi <nicolas.ogi@heig-vd.ch>
2021-03-21 17:04:19 +0100

    Adding errors handling

--- specs/NicolasOgi/server/src/main/java/Server.java
+++ specs/NicolasOgi/server/src/main/java/Server.java
@@ -1,17 +1,26 @@
 package main.java;
 
 import java.io.*;
-import java.net.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
 
     private final static String WELCOME_MESSAGE = "WELCOME \r\n" +
-                                                  "- AVAILABLE OPERATIONS \r\n" +
-                                                  "- ADD <N1> <N2> \r\n" +
-                                                  "- MULT <N1> <N2> \r\n" +
-                                                  "- DIV <N1> <N2> \r\n" +
-                                                  "- END OPERATIONS \r\n";
+            "- AVAILABLE OPERATIONS \r\n" +
+            "- ADD <N1> <N2> \r\n" +
+            "- MULT <N1> <N2> \r\n" +
+            "- DIV <N1> <N2> \r\n" +
+            "- END OPERATIONS \r\n";
 
+    public static void main(String[] args) throws IOException {
+
+        Server server = new Server();
+        while (true) {
+            server.waitForIncomingClient();
+        }
+
+    }
 
     public void waitForIncomingClient() throws IOException {
 
@@ -22,51 +31,63 @@ public class Server {
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         PrintWriter out = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
 
-        // Envoie du message de bienvenue au client
+        // Envoi du message de bienvenue au client
         out.println(WELCOME_MESSAGE);
         out.flush();
 
         String clientMessage = in.readLine();
-        while (clientMessage != null) {
-            String[] tokens = clientMessage.split(" ");
+
+        String[] tokens = clientMessage.split(" ");
+
+        try {
+
             String command = tokens[0];
             String operation = tokens[1];
             int operand1 = Integer.parseInt(tokens[2]);
             int operand2 = Integer.parseInt(tokens[3]);
-            int result;
+            int result = 0;
+            boolean error = false;
+
+            if (tokens.length != 4)
+                throw new IllegalArgumentException("TOO MUCH ARGUMENTS");
 
             if (command.equals("COMPUTE")) {
 
-                if (operation.equals("ADD")) {
-                    result = operand1 + operand2;
+                switch (operation) {
+                    case "ADD":
+                        result = operand1 + operand2;
+                        break;
+                    case "MULT":
+                        result = operand1 * operand2;
+                        break;
+                    case "DIV":
+                        result = operand1 / operand2;
+                        break;
+                    default:
+                        out.println("ERROR 300 UNKNOWN OPERATION \r\n");
+                        error = true;
+                        break;
                 }
-                else if (operation.equals("MULT")) {
-                    result = operand1 * operand2;
-                }
-                else if (operation.equals("DIV")) {
-                    result = operand1 / operand2;
-                }
-                else {
-                    out.println("ERROR 300 UNKNOWN OPERATION \r\n");
-                    break;
-                }
-                out.println("RESULT " + result + "\r\n");
+
+                if (!error)
+                    out.println("RESULT " + result + "\r\n");
+
+                out.flush();
+            }
+            else {
+                out.println("ERROR 900 UNKNOWN COMMAND \r\n");
                 out.flush();
             }
         }
-        in.close();
-        out.close();
-        clientSocket.close();
-        receptionistSocket.close();
-
-    }
-
-    public static void main(String[] args) throws IOException {
-
-        Server server = new Server();
-        while (true) {
-            server.waitForIncomingClient();
+        catch(Exception e) { // Catch toutes les exceptions pour l'instant...
+            out.println("ERROR : " + e.getMessage() + "\r\n");
+            out.flush();
         }
-
+        finally {
+            in.close();
+            out.close();
+            clientSocket.close();
+            receptionistSocket.close();
+        }
     }
 }
